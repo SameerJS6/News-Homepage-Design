987a57b532dbc6005fb622250f34abe83c0f8431
Sameer Singh <sameersingh1234pok@gmail.com>
2022-11-04 16:47:54 +0530

    Ripple Effect Added

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
-let Menu = document.querySelector(".menu");
-let Nav = document.querySelector(".navbar");
+const Menu = document.querySelector(".menu");
+const Nav = document.querySelector(".navbar");
 const Main = document.querySelector(".main")
+const Ripple = document.querySelectorAll(".ripple");
 
 Menu.addEventListener("click", ()=> {
     Nav.classList.toggle("active");
     Menu.classList.toggle('close');
     Main.toggleAttribute('data-overlay');
     document.body.classList.toggle("scroll-hidden");
-})
+});
+
+Ripple.forEach((ripple) => {
+    ripple.addEventListener("click", function(e) {
+        let x = e.clientX - e.target.offsetLeft;
+        let y = e.clientY - e.target.offsetTop;
+
+        let Ripples = document.createElement('span');
+        Ripples.style.left = x + "px";
+        Ripples.style.top = y + "px";
+        this.appendChild(Ripples);
+
+        setTimeout(() => {
+            Ripples.remove()
+        }, 10000);
+    });
+});
